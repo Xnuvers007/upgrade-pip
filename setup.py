a13cac3ecb1e9e63eace0033d804dba8b2bd4b3a
xnuvers007 <xnuvers007@kali.xnuvers007>
2022-08-24 02:11:08 +0700

    Update and fix typo

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.1.3'
+VERSION = '0.1.4a'
 DESCRIPTION = 'upgrade packages pip outdated'
 LONG_DESCRIPTION = 'A package that allows to upgrade all pip python outdated.'
 
@@ -18,6 +18,7 @@ setup(
     author="Xnuvers007 (Indra Dwi A)",
     author_email="indra111.ida@gmail.com",
     description=DESCRIPTION,
+    license_files = ('LICENSE',),
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
@@ -37,4 +38,3 @@ setup(
         "Natural Language :: Indonesian"
     ]
 )
-
